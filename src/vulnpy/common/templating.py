8438010273ccad5bc90503c2dcfdbaff35c802db
Peter Lazorchak <peter.lazorchak@contrastsecurity.com>
2020-08-19 10:20:28 -0400

    PYT-897 Add cache to template disk reads

--- src/vulnpy/common/templating.py
+++ src/vulnpy/common/templating.py
@@ -5,6 +5,18 @@ import vulnpy
 TEMPLATES_LOCATION = os.path.join(os.path.dirname(vulnpy.__file__), "templates")
 
 
+def cache(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+
+    return wrapper
+
+
+@cache
 def get_template(path):
     """
     Read and return the contents of the file at TEMPLATES_LOCATON/<path>.
