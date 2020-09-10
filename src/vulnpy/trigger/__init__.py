c5725b80ed0a3fd4fed017f71042e3f49740d7fa
Dani <dani.alcala@contrastsecurity.com>
2020-09-10 14:39:55 -0500

    autogenerate django trigger urls

---
+++ src/vulnpy/trigger/__init__.py
@@ -0,0 +1,13 @@
+TRIGGER_MAP = {
+    "home": [],
+    "cmdi": [
+        "os-system",
+        "subprocess-popen",
+    ],
+    "deserialization": [
+        "pickle-load",
+        "pickle-loads",
+        "yaml-load",
+        "yaml-load-all",
+    ],
+}
