3c9cc4c43344e968f067709871b61a3e30b8cd6b
Dani <dani.alcala@contrastsecurity.com>
2020-09-10 15:32:43 -0500

    automate flask root urls

--- src/vulnpy/trigger/__init__.py
+++ src/vulnpy/trigger/__init__.py
@@ -11,3 +11,19 @@ TRIGGER_MAP = {
         "yaml-load-all",
     ],
 }
+
+
+def get_trigger(module, trigger_name):
+    """
+    Find a function LIKE trigger_name in the module.
+
+    :param module: Python module
+    :param trigger_name: str
+    :return: function
+    """
+    func_name = "do_{}".format(trigger_name.replace("-", "_"))
+
+    try:
+        return getattr(module, func_name)
+    except AttributeError:
+        return
