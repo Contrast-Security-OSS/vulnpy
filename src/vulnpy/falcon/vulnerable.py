3fa943aa51d297b33589b1307c80ea563cd7bee4
Dani <dani.alcala@contrastsecurity.com>
2020-09-11 09:00:01 -0500

    automate generating falcon routes

--- src/vulnpy/falcon/vulnerable.py
+++ src/vulnpy/falcon/vulnerable.py
@@ -1,5 +1,98 @@
+import inspect
+import sys
 from vulnpy.common import get_template
-from vulnpy.trigger import cmdi, deserialization
+from vulnpy.trigger import TRIGGER_MAP, get_trigger, cmdi, deserialization  # noqa: F401
+
+
+def get_root_name(name):
+    if name == "home":
+        return "/vulnpy"
+    return "/vulnpy/{}".format(name)
+
+
+def get_trigger_name(name, trigger):
+    return "/vulnpy/{}/{}".format(name, trigger)
+
+
+def gen_root_view(name):  # noqa: C901
+    if name == "home":
+
+        class _View(object):
+            def on_get(self, req, resp):
+                _set_response(resp, "{}.html".format(name))
+
+    else:
+
+        class _View(object):
+            def trigger(self, command):
+                pass
+
+            def on_get(self, req, resp):
+                user_input = req.get_param("user_input") or ""
+                catch_exception = req.get_param_as_bool("catch_exception")
+
+                if catch_exception:
+                    try:
+                        self.trigger(user_input)
+                    except Exception:
+                        pass
+                else:
+                    self.trigger(user_input)
+
+                _set_response(resp, "{}.html".format(name))
+
+    return _View
+
+
+def find_base_class(name):
+    """
+    Find a class LIKE name currently defined in this module.
+
+    :param name: str
+    :return: class matching "name"
+    """
+    current_module = sys.modules[__name__]
+
+    for obj_name, obj in inspect.getmembers(current_module):
+        if inspect.isclass(obj) and obj_name == name.title():
+            return obj
+
+
+def get_trigger_view(name, trigger):
+
+    baseclass = find_base_class(name)
+
+    class _View(baseclass):
+        def trigger(self, command):
+            module = sys.modules.get("vulnpy.trigger.{}".format(name))
+            trigger_func = get_trigger(module, trigger)
+
+            if trigger_func:
+                trigger_func(command)
+
+    return _View
+
+
+def generate_root_urls(app):
+    for name in TRIGGER_MAP:
+        view_name = get_root_name(name)
+        view_cls = gen_root_view(name)
+
+        # Add the view class to the current module to be able to retrieve later
+        current_module = sys.modules[__name__]
+        setattr(current_module, name.title(), view_cls)
+
+        app.add_route(view_name, view_cls())
+
+
+def generate_trigger_urls(app):
+    for name, triggers in TRIGGER_MAP.items():
+        for trigger in triggers:
+            view_name = get_trigger_name(name, trigger)
+
+            view_cls = get_trigger_view(name, trigger)
+
+            app.add_route(view_name, view_cls())
 
 
 def add_vulnerable_routes(app):
@@ -10,18 +103,10 @@ def add_vulnerable_routes(app):
     :param app: instance of falcon.API
     """
     app.req_options.strip_url_path_trailing_slash = True
-    app.add_route("/vulnpy", Home())
 
-    app.add_route("/vulnpy/cmdi", Cmdi())
-    app.add_route("/vulnpy/cmdi/os-system", OsSystem())
-    app.add_route("/vulnpy/cmdi/subprocess-popen", SubprocessPopen())
-
-    app.add_route("/vulnpy/deserialization", Deserialization())
-    app.add_route("/vulnpy/deserialization/pickle-load", PickleLoad())
-    app.add_route("/vulnpy/deserialization/pickle-loads", PickleLoads())
-
-    app.add_route("/vulnpy/deserialization/yaml-load", YamlLoad())
-    app.add_route("/vulnpy/deserialization/yaml-load-all", YamlLoadAll())
+    # Root urls must be generated before trigger urls.
+    generate_root_urls(app)
+    generate_trigger_urls(app)
 
 
 def _set_response(resp, path):
@@ -30,67 +115,3 @@ def _set_response(resp, path):
     """
     resp.body = get_template(path)
     resp.content_type = "text/html"
-
-
-class Home(object):
-    def on_get(self, req, resp):
-        _set_response(resp, "home.html")
-
-
-class Cmdi(object):
-    def trigger(self, command):
-        pass
-
-    def on_get(self, req, resp):
-        user_input = req.get_param("user_input") or ""
-        catch_exception = req.get_param_as_bool("catch_exception")
-
-        if catch_exception:
-            try:
-                self.trigger(user_input)
-            except Exception:
-                pass
-        else:
-            self.trigger(user_input)
-
-        _set_response(resp, "cmdi.html")
-
-
-class OsSystem(Cmdi):
-    def trigger(self, command):
-        cmdi.do_os_system(command)
-
-
-class SubprocessPopen(Cmdi):
-    def trigger(self, command):
-        cmdi.do_subprocess_popen(command)
-
-
-class Deserialization(object):
-    def trigger(self, command):
-        pass
-
-    def on_get(self, req, resp):
-        user_input = req.get_param("user_input") or ""
-        self.trigger(user_input)
-        _set_response(resp, "deserialization.html")
-
-
-class PickleLoad(Deserialization):
-    def trigger(self, command):
-        deserialization.do_pickle_load(command)
-
-
-class PickleLoads(Deserialization):
-    def trigger(self, command):
-        deserialization.do_pickle_loads(command)
-
-
-class YamlLoad(Deserialization):
-    def trigger(self, command):
-        deserialization.do_yaml_load(command)
-
-
-class YamlLoadAll(Deserialization):
-    def trigger(self, command):
-        deserialization.do_yaml_load_all(command)
