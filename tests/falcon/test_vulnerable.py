a85c5c77f9feb4f5526dc0d4df52751e12959ec3
Dani <dani.alcala@contrastsecurity.com>
2020-09-16 09:13:50 -0500

    parametrize falcon tests

--- tests/falcon/test_vulnerable.py
+++ tests/falcon/test_vulnerable.py
@@ -8,6 +8,9 @@ from falcon import testing
 import vulnpy
 import vulnpy.falcon
 
+from vulnpy.trigger import DATA
+from tests import parametrize_root, parametrize_triggers
+
 
 @pytest.fixture(scope="module")
 def client():
@@ -16,54 +19,28 @@ def client():
     return testing.TestClient(app)
 
 
-def test_home(client):
-    response = client.simulate_get("/vulnpy")
+@parametrize_root
+def test_root_views(client, view_name):
+    if view_name == "home":
+        response = client.simulate_get("/vulnpy")
+    else:
+        response = client.simulate_get("/vulnpy/{}".format(view_name))
     assert response.status_code == 200
 
 
-def test_cmdi_basic(client):
-    response = client.simulate_get("/vulnpy/cmdi")
-    assert response.status_code == 200
+@parametrize_triggers
+@pytest.mark.parametrize("request_method", ["simulate_get"])
+def test_trigger(client, request_method, view_name, trigger_name):
+    get_or_post = getattr(client, request_method)
 
+    data = DATA.get(view_name)
 
-def test_cmdi_os_system_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/cmdi/os-system", params={"user_input": "echo attack"}
-    )
-    assert response.status_code == 200
+    if view_name == "unsafe_code_exec":
+        data = quote(data)
 
-
-def test_cmdi_os_system_bad_command(client):
-    response = client.simulate_get(
-        "/vulnpy/cmdi/os-system", params={"user_input": "foo"}
-    )
-    assert response.status_code == 200
-
-
-def test_cmdi_os_system_invalid_input(client):
-    response = client.simulate_get(
-        "/vulnpy/cmdi/os-system", params={"ignored_param": "bad"}
-    )
-    assert response.status_code == 200
-
-
-def test_cmdi_subprocess_popen_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/cmdi/subprocess-popen", params={"user_input": "echo attack"}
-    )
-    assert response.status_code == 200
-
-
-def test_cmdi_subprocess_popen_bad_command(client):
-    response = client.simulate_get(
-        "/vulnpy/cmdi/subprocess-popen", params={"user_input": "foo"}
-    )
-    assert response.status_code == 200
-
-
-def test_cmdi_subprocess_popen_invalid_input(client):
-    response = client.simulate_get(
-        "/vulnpy/cmdi/subprocess-popen", params={"ignored_param": "bad"}
+    response = get_or_post(
+        "/vulnpy/{}/{}".format(view_name, trigger_name),
+        params={"user_input": data},
     )
     assert response.status_code == 200
 
@@ -79,76 +56,3 @@ def test_handle_exception(mocked_trigger, client):
     assert mocked_trigger.called
     assert response.status_code == 200
 
-
-def test_deserialization_basic(client):
-    response = client.simulate_get("/vulnpy/deserialization")
-    assert response.status_code == 200
-
-
-def test_deserialization_pickle_load_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/deserialization/pickle-load",
-        params={"user_input": "csubprocess\ncheck_output\n(S'ls'\ntR."},
-    )
-    assert response.status_code == 200
-
-
-def test_deserialization_pickle_loads_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/deserialization/pickle-loads",
-        params={"user_input": "csubprocess\ncheck_output\n(S'ls'\ntR."},
-    )
-    assert response.status_code == 200
-
-
-def test_deserialization_yaml_load_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/deserialization/yaml-load",
-        params={
-            "user_input": '!!python/object/apply:subprocess.Popen [["echo", "Hello World"]]'
-        },
-    )
-    assert response.status_code == 200
-
-
-def test_deserialization_yaml_load_all_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/deserialization/yaml-load-all",
-        params={
-            "user_input": '!!python/object/apply:subprocess.Popen [["echo", "Hello World"]]'
-        },
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("endpoint", ["eval", "exec", "compile"])
-def test_unsafe_code_exec_normal(client, endpoint):
-    response = client.simulate_get(
-        "/vulnpy/unsafe_code_exec/{}".format(endpoint),
-        params={"user_input": quote("1 + 2")},
-    )
-    assert response.status_code == 200
-
-
-def test_xxe_lxml_etree_fromstring_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/xxe/lxml-etree-fromstring",
-        params={"user_input": "<root>attack</root>"},
-    )
-    assert response.status_code == 200
-
-
-def test_xxe_xml_dom_pulldom_parsestring_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/xxe/xml-dom-pulldom-parsestring",
-        params={"user_input": "<root>attack</root>"},
-    )
-    assert response.status_code == 200
-
-
-def test_xxe_xml_sax_parsestring_normal(client):
-    response = client.simulate_get(
-        "/vulnpy/xxe/xml-sax-parsestring",
-        params={"user_input": "<root>attack</root>"},
-    )
-    assert response.status_code == 200
