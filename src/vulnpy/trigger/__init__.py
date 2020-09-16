ed3ce8f63446cb863605b264f8fc661fa004d7d6
Dani <dani.alcala@contrastsecurity.com>
2020-09-16 08:41:29 -0500

    parametrize django tests

--- src/vulnpy/trigger/__init__.py
+++ src/vulnpy/trigger/__init__.py
@@ -1,3 +1,10 @@
 from vulnpy.trigger.util import create_trigger_map, get_trigger  # noqa: F401
 
 TRIGGER_MAP = create_trigger_map()
+
+DATA = {
+    "cmdi": "echo attack",
+    "deserialization": "csubprocess\ncheck_output\n(S'ls'\ntR.",  # TODO yaml data
+    "unsafe_code_exec": "1 + 2",
+    "xxe": "<root>attack</root>",
+}
--- tests/django/test_vulnerable.py
+++ tests/django/test_vulnerable.py
@@ -2,6 +2,17 @@ import pytest
 from django.conf import settings
 from django.test import Client
 
+from vulnpy.trigger import TRIGGER_MAP, DATA
+
+vuln_names = [x for x in TRIGGER_MAP if x != "util"]
+
+arglist = []
+for vuln_name, trigger_names in TRIGGER_MAP.items():
+    if not trigger_names:
+        continue
+    for trigger in trigger_names:
+        arglist.append((vuln_name, trigger))
+
 
 @pytest.fixture(scope="module")
 def client():
@@ -11,136 +22,21 @@ def client():
     return Client()
 
 
-def test_vulnpy_root(client):
-    response = client.get("/vulnpy")
+@pytest.mark.parametrize("view_name", vuln_names)
+def test_root_views(client, view_name):
+    if view_name == "home":
+        response = client.get("/vulnpy")
+    else:
+        response = client.get("/vulnpy/{}".format(view_name))
     assert response.status_code == 200
 
 
-def test_cmdi_basic(client):
-    response = client.get("/vulnpy/cmdi")
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_cmdi_os_system_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post("/vulnpy/cmdi/os-system", {"user_input": "echo attack"})
-    assert response.status_code == 200
-
-
-def test_cmdi_os_system_bad_command(client):
-    response = client.get("/vulnpy/cmdi/os-system", {"user_input": "foo"})
-    assert response.status_code == 200
-
-
-def test_cmdi_os_system_invalid_input(client):
-    response = client.get("/vulnpy/cmdi/os-system", {"ignored_param": "bad"})
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_cmdi_subprocess_popen_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
+@pytest.mark.parametrize("view_name,trigger_name", arglist)
+@pytest.mark.parametrize("request_method", ["get", "post"])
+def test_trigger(client, request_method, view_name, trigger_name):
+    get_or_post = getattr(client, request_method)
     response = get_or_post(
-        "/vulnpy/cmdi/subprocess-popen", {"user_input": "echo attack"}
-    )
-    assert response.status_code == 200
-
-
-def test_cmdi_subprocess_popen_bad_command(client):
-    response = client.get("/vulnpy/cmdi/subprocess-popen", {"user_input": "foo"})
-    assert response.status_code == 200
-
-
-def test_cmdi_subprocess_popen_invalid_input(client):
-    response = client.get("/vulnpy/cmdi/subprocess-popen", {"ignored_param": "bad"})
-    assert response.status_code == 200
-
-
-def test_deserialization_basic(client):
-    response = client.get("/vulnpy/deserialization")
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_deserialization_pickle_load_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/deserialization/pickle-load",
-        {"user_input": "csubprocess\ncheck_output\n(S'ls'\ntR."},
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_deserialization_pickle_loads_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/deserialization/pickle-loads",
-        {"user_input": "csubprocess\ncheck_output\n(S'ls'\ntR."},
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_deserialization_yaml_load_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/deserialization/yaml-load",
-        {
-            "user_input": '!!python/object/apply:subprocess.Popen [["echo", "Hello World"]]'
-        },
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_deserialization_yaml_load_all_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/deserialization/yaml-load-all",
-        {
-            "user_input": '!!python/object/apply:subprocess.Popen [["echo", "Hello World"]]'
-        },
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-@pytest.mark.parametrize("endpoint", ["eval", "exec", "compile"])
-def test_unsafe_code_exec_normal(client, method_name, endpoint):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/unsafe_code_exec/{}/".format(endpoint), {"user_input": "1 + 2"}
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_xxe_lxml_etree_fromstring_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/xxe/lxml-etree-fromstring",
-        {"user_input": "<root>attack</root>"},
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_xxe_xml_dom_pulldom_parsestring_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/xxe/xml-dom-pulldom-parsestring",
-        {"user_input": "<root>attack</root>"},
-    )
-    assert response.status_code == 200
-
-
-@pytest.mark.parametrize("method_name", ["get", "post"])
-def test_xxe_xml_sax_parsestring_normal(client, method_name):
-    get_or_post = getattr(client, method_name)
-    response = get_or_post(
-        "/vulnpy/xxe/xml-sax-parsestring",
-        {"user_input": "<root>attack</root>"},
+        "/vulnpy/{}/{}".format(view_name, trigger_name),
+        {"user_input": DATA.get(view_name)},
     )
     assert response.status_code == 200
