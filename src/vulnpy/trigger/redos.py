0ea97a44d9d46475e4c63a7fd83f147459f1d043
Dani <dani.alcala@contrastsecurity.com>
2020-12-16 08:40:01 -0600

    update redos triggers

--- src/vulnpy/trigger/redos.py
+++ src/vulnpy/trigger/redos.py
@@ -2,17 +2,12 @@ import re
 
 
 def do_re_match(user_input):
-    match = re.match(r"\(a*", user_input)
+    match = re.match(r"((a)+)+", user_input)
     if match:
         return match.group(0)
 
 
 def do_re_search(user_input):
-    match = re.search(r"\(a*", user_input)
+    match = re.search(r"((a)+)+", user_input)
     if match:
         return match.group(0)
-
-
-def do_re_finditer(user_input):
-    for match in re.finditer(r"\(a*", user_input):
-        match.group(0)
