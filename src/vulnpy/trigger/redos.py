97d79b1747e5b912f753c99cf30413a1bd5463e0
Dani <dani.alcala@contrastsecurity.com>
2021-01-05 13:06:39 -0800

    add more redos triggers

--- src/vulnpy/trigger/redos.py
+++ src/vulnpy/trigger/redos.py
@@ -1,4 +1,5 @@
 import re
+from vulnpy.vendor import six
 
 
 def do_re_match(user_input):
@@ -11,3 +12,34 @@ def do_re_search(user_input):
     match = re.search(r"((a)+)+", user_input)
     if match:
         return match.group(0)
+
+
+def do_re_finditer(user_input):
+    for match in re.finditer(r"((a)+)+", user_input):
+        match.group(0)
+
+
+def do_re_findall(user_input):
+    return re.findall(r"((a)+)+", user_input)
+
+
+def do_re_fullmatch(user_input):
+    if six.PY2:
+        # fulmmatch is Py3 only
+        return None
+
+    match = re.fullmatch(r"((a)+)+", user_input)
+    if match:
+        return match.group(0)
+
+
+def do_re_sub(user_input):
+    return re.sub(r"((a)+)+", "anything", user_input)
+
+
+def do_re_subn(user_input):
+    return re.subn(r"((a)+)+", "anything", user_input)
+
+
+def do_re_split(user_input):
+    return re.split(r"((a)+)+", user_input)
