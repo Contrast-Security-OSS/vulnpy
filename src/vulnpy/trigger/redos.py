dfb49a841375b39c847c7ec9c4970a1fc519db48
Dani <dani.alcala@contrastsecurity.com>
2021-01-06 11:09:59 -0800

    add re.Pattern.method(s) triggers

--- src/vulnpy/trigger/redos.py
+++ src/vulnpy/trigger/redos.py
@@ -1,26 +1,53 @@
 import re
 from vulnpy.vendor import six
 
+PATTERN = r"((a)+)+"
+
 
 def do_re_match(user_input):
-    match = re.match(r"((a)+)+", user_input)
+    match = re.match(PATTERN, user_input)
+    if match:
+        return match.group(0)
+
+
+def do_re_match_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    match = pattern.match(user_input)
     if match:
         return match.group(0)
 
 
 def do_re_search(user_input):
-    match = re.search(r"((a)+)+", user_input)
+    match = re.search(PATTERN, user_input)
+    if match:
+        return match.group(0)
+
+
+def do_re_search_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    match = pattern.search(user_input)
     if match:
         return match.group(0)
 
 
 def do_re_finditer(user_input):
-    for match in re.finditer(r"((a)+)+", user_input):
+    for match in re.finditer(PATTERN, user_input):
+        match.group(0)
+
+
+def do_re_finditer_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    for match in pattern.finditer(user_input):
         match.group(0)
 
 
 def do_re_findall(user_input):
-    return re.findall(r"((a)+)+", user_input)
+    return re.findall(PATTERN, user_input)
+
+
+def do_re_findall_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    return pattern.findall(user_input)
 
 
 def do_re_fullmatch(user_input):
@@ -28,18 +55,44 @@ def do_re_fullmatch(user_input):
         # fulmmatch is Py3 only
         return None
 
-    match = re.fullmatch(r"((a)+)+", user_input)
+    match = re.fullmatch(PATTERN, user_input)
+    if match:
+        return match.group(0)
+
+
+def do_re_fullmatch_compiled(user_input):
+    if six.PY2:
+        # fulmmatch is Py3 only
+        return None
+
+    pattern = re.compile(PATTERN)
+    match = pattern.fullmatch(user_input)
     if match:
         return match.group(0)
 
 
 def do_re_sub(user_input):
-    return re.sub(r"((a)+)+", "anything", user_input)
+    return re.sub(PATTERN, "anything", user_input)
+
+
+def do_re_sub_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    return pattern.sub("anything", user_input)
 
 
 def do_re_subn(user_input):
-    return re.subn(r"((a)+)+", "anything", user_input)
+    return re.subn(PATTERN, "anything", user_input)
+
+
+def do_re_subn_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    return pattern.subn("anything", user_input)
 
 
 def do_re_split(user_input):
-    return re.split(r"((a)+)+", user_input)
+    return re.split(PATTERN, user_input)
+
+
+def do_re_split_compiled(user_input):
+    pattern = re.compile(PATTERN)
+    return pattern.split(user_input)
