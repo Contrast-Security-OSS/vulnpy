7acc193d5b9cf3de5a63f5751ecc7658e0ff0586
ivaylo.evtimov <ivaylo.evtimov@contrastsecurity.com>
2021-07-09 18:34:48 +0300

    Added a fix for mongodb client inside nosqli trigger.

--- src/vulnpy/trigger/nosqli.py
+++ src/vulnpy/trigger/nosqli.py
@@ -2,19 +2,21 @@ from pymongo import MongoClient
 import os
 
 
+mongo_host = [os.environ.get("MONGODB_HOST", "localhost:27017")]
+
+
 def mongo_db_exists():
-    client = MongoClient(serverSelectionTimeoutMS=20)
-    db = client.pymongo_test
+    test_client = MongoClient(host=mongo_host, serverSelectionTimeoutMS=20)
+    test_db = test_client.pymongo_test
 
     try:
-        db.posts.count_documents({})
+        test_db.posts.count_documents({})
     except Exception:  # pragma: no cover
         return False
     return True
 
 
 def create_mongo_client(**kwargs):
-    mongo_host = [os.environ.get("MONGODB_HOST", "localhost:27017")]
     if mongo_db_exists():
         return MongoClient(host=mongo_host, **kwargs)
     else:
