1a121d2b4f3e3844a472e53149a99ff336378929
Peter Lazorchak <peter.lazorchak@contrastsecurity.com>
2021-08-04 14:14:36 -0700

    Add primary app to flask-two-apps

--- apps/flask_two_apps.py
+++ apps/flask_two_apps.py
@@ -6,12 +6,15 @@ from vulnpy.flask.blueprint import vulnerable_blueprint
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 
-app1 = Flask(__name__)
+primary_app = Flask(__name__ + "-primary")
+
+app1 = Flask(__name__ + "-1")
 app1.register_blueprint(vulnerable_blueprint)
 
 
-app2 = Flask(__name__)
+app2 = Flask(__name__ + "-2")
 app2.register_blueprint(vulnerable_blueprint)
+
 
 @app1.route("/")
 def frontend():
@@ -19,7 +22,7 @@ def frontend():
     return redirect("/vulnpy/")
 
 
-@app1.route("/backend")
+@app1.route("/backend")  # maybe this was supposed to be app2?
 def backend():
     breakpoint()
     return redirect("/vulnpy/")
@@ -31,7 +34,8 @@ if os.environ.get("VULNPY_USE_CONTRAST"):
     app1.wsgi_app = ContrastMiddleware(app1)
     app2.wsgi_app = ContrastMiddleware(app2)
 
-    combined_app = DispatcherMiddleware(app1, {
-        '/app2': app2
-    })
-    combined_app = combined_app.app
+    primary_app.wsgi_app = DispatcherMiddleware(
+        primary_app.wsgi_app, {"/app1": app1.wsgi_app, "/app2": app2.wsgi_app}
+    )
+
+    combined_app = primary_app
