10db6daea863871008c360599f6d82532114533f
Peter Lazorchak <peter.lazorchak@contrastsecurity.com>
2021-09-07 12:46:26 -0700

    PYT-1697 Enable alternate WSGI servers for django app

--- apps/django_app.py
+++ apps/django_app.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-import django
 from django.conf import settings
-from django.core.management import execute_from_command_line
+from django.core import management
+from django.core.wsgi import get_wsgi_application
 from django.shortcuts import redirect
 
 try:
@@ -18,28 +18,22 @@ urlpatterns = [
     compat_url(r"^$", lambda r: redirect("/vulnpy"))
 ] + vulnerable_urlpatterns
 
-filename = os.path.basename(os.path.splitext(__file__)[0])
+if not settings.configured:
+    settings.configure(
+        **{
+            "ROOT_URLCONF": "django_app"
+            if __name__ == "__main__"
+            else "apps.django_app",
+            "ALLOWED_HOSTS": ["localhost", "127.0.0.1", "[::1]"],
+        }
+    )
+
+application = get_wsgi_application()
+
+if os.environ.get("VULNPY_USE_CONTRAST"):
+    from contrast.django import ContrastMiddleware
+
+    application = ContrastMiddleware(application)
 
 if __name__ == "__main__":
-
-    options = {
-        "ROOT_URLCONF": filename,
-        "ALLOWED_HOSTS": ["localhost", "127.0.0.1", "[::1]"],
-    }
-
-    if os.environ.get("VULNPY_USE_CONTRAST"):
-        # TODO: be able to import as "contrast.django.ContrastMiddleware"
-        if django.VERSION < (1, 10):
-            middleware_setting_name = "MIDDLEWARE_CLASSES"
-            contrast_middleware_name = (
-                "contrast.agent.middlewares.legacy_django_middleware.DjangoMiddleware"
-            )
-        else:
-            middleware_setting_name = "MIDDLEWARE"
-            contrast_middleware_name = (
-                "contrast.agent.middlewares.django_middleware.DjangoMiddleware"
-            )
-        options[middleware_setting_name] = [contrast_middleware_name]
-
-    settings.configure(**options)
-    execute_from_command_line(sys.argv)
+    management.execute_from_command_line(sys.argv)
