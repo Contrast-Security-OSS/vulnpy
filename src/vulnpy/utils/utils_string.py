1d3147f7bfe52be567af7c54c8220976a67adbee
angelobretenovv <angel.obretenov@contrastsecurity.com>
2022-01-07 14:10:39 +0200

    Removed 'six'

---
+++ src/vulnpy/utils/utils_string.py
@@ -0,0 +1,16 @@
+def ensure_binary(s, encoding="utf-8", errors="strict"):
+    if isinstance(s, bytes):
+        return s
+    if isinstance(s, str):
+        return s.encode(encoding, errors)
+    raise TypeError(f"not expecting type '{type(s)}'")
+
+
+def ensure_str(s, encoding="utf-8", errors="strict"):
+    if type(s) is str:
+        return s
+    if isinstance(s, bytes):
+        return s.decode(encoding, errors)
+    elif not isinstance(s, (str, bytes)):
+        raise TypeError(f"not expecting type '{type(s)}'")
+    return s
