3cdc142fed9c9dc345145bfe8e25dc0c7f6c5b66
Dani <dani.alcala@contrastsecurity.com>
2022-02-10 11:44:44 -0800

    add blueprint to quart app

--- apps/quart_app.py
+++ apps/quart_app.py
@@ -1,9 +1,19 @@
-from quart import Quart
+import sys
+
+from quart import Quart, redirect
+from vulnpy.quart import vulnerable_blueprint
+
 
 app = Quart(__name__)
+app.register_blueprint(vulnerable_blueprint)
 
-@app.route('/')
-async def hello():
-    return 'hello'
+@app.route("/")
+async def index():
+    return redirect("/vulnpy/")
 
-app.run()
+
+if __name__ == "__main__":
+    host = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 8000
+
+    app.run(host, port)
---
+++ src/vulnpy/quart/blueprint.py
@@ -0,0 +1,74 @@
+from quart import Blueprint, request
+
+from vulnpy.common import get_template
+from vulnpy.trigger import TRIGGER_MAP, get_trigger
+
+vulnerable_blueprint = Blueprint("vulnpy", __name__, url_prefix="/vulnpy")
+
+
+def _get_user_input():
+    if request.headers.get("User-Input") is not None:
+        return request.headers.get("User-Input")
+
+    if request.method == "GET":
+        return request.args.get("user_input", "")
+    return request.form.get("user_input", "")
+
+
+def get_root_name(name):
+    if name == "home":
+        return "/"
+    return "/{}/".format(name)
+
+
+def get_trigger_name(name, trigger):
+    return "/{}/{}/".format(name, trigger)
+
+
+def gen_root_view(name):
+    async def _view():
+        return get_template("{}.html".format(name))
+
+    view_name = get_root_name(name)
+    vulnerable_blueprint.add_url_rule(
+        view_name, view_name, _view, methods=["GET", "POST"], strict_slashes=False
+    )
+    return _view
+
+
+def generate_root_urls():
+    for name in TRIGGER_MAP:
+        gen_root_view(name)
+
+
+def get_trigger_view(name, trigger):
+    async def _view():
+        user_input = _get_user_input()
+        trigger_func = get_trigger(name, trigger)
+
+        if trigger_func:
+            trigger_func(user_input)
+
+        template = get_template("{}.html".format(name))
+
+        if name == "xss" and trigger == "raw":
+            template += "<p>XSS: " + user_input + "</p>"
+
+        return template
+
+    view_name = get_trigger_name(name, trigger)
+
+    vulnerable_blueprint.add_url_rule(
+        view_name, view_name, _view, methods=["GET", "POST"], strict_slashes=False
+    )
+    return _view
+
+
+def generate_trigger_urls():
+    for name, triggers in TRIGGER_MAP.items():
+        for trigger in triggers:
+            get_trigger_view(name, trigger)
+
+
+generate_root_urls()
+generate_trigger_urls()
