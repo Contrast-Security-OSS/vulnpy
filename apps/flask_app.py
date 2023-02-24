81f7c666e35f66394e66612b81223edc88179bc6
Daniel D'Avella <daniel.davella@contrastsecurity.com>
2023-02-24 11:18:50 -0500

    Add jinja template xss to flask app

--- apps/flask_app.py
+++ apps/flask_app.py
@@ -1,11 +1,29 @@
 import os
+from pathlib import Path
 
-from flask import Flask, redirect
+from flask import Blueprint, Flask, redirect, request
+from jinja2 import Environment, FileSystemLoader
 
 from vulnpy.flask.blueprint import vulnerable_blueprint
 
+
+loader = FileSystemLoader(Path(__file__).parent / "templates" / "jinja")
+environment = Environment(loader=loader)
+
+template_blueprint = Blueprint("template-xss", __name__, url_prefix="/template-xss")
+
+
+@template_blueprint.get("/jinja")
+def jinja_xss():
+    template = environment.get_template("xss.html")
+    return template.render(
+        path="/template-xss/jinja", user_input=request.args.get("user_input", "")
+    )
+
+
 app = Flask(__name__)
 app.register_blueprint(vulnerable_blueprint)
+app.register_blueprint(template_blueprint)
 
 
 app.config["SESSION_COOKIE_SECURE"] = False
